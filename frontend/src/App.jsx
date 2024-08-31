06c023527ab1a243394a831889f6be8dc634cdf2
Roshan-0001 <roshan.sharma6383@gmail.com>
2024-08-31 19:31:43 +0530

    otp configuration added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-import { FormOTP , Form } from './Form';
+import { FormOTP , Form, OtpResponse } from './Form';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -21,7 +21,7 @@ function App() {
 
       <Routes>
         <Route path="/register" element={<Form/>} />
-        <Route path="/register/send-otp" element={<FormOTP/>} />
+        <Route path="/register/send-otp" element={<OtpResponse/>} />
         <Route path="/register/verify-otp" element={<FormOTP/>} />
       </Routes>
     </Router>
--- frontend/src/Form.jsx
+++ frontend/src/Form.jsx
@@ -1,26 +1,27 @@
 import "./Form.css";
 import React from "react";
 import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from 'react-router-dom';
 
-function Form(){
-    const [formData, setFormData] = useState({username: '', email: '', password: ''})
-    const [data, setData] = useState(null);
-    const [msg, setMsg] = useState("Loding");
 
-    
+function Form() {
+    const [formData, setFormData] = useState({ username: '', email: '', password: '' })
+    const [msg, setMsg] = useState(null);
 
-    const handleChange = (e) =>{
-        setFormData({...formData, [e.target.id]: e.target.value});
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
     }
 
-    const handleSubmit = async (e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
-            
-            const response = await fetch('/api/register' , {
+
+            const response = await fetch('/api/register', {
                 method: 'POST',
-                headers:{
+                headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(formData),
@@ -28,103 +29,157 @@ function Form(){
 
             const result = await response.json();
 
-            if(response.ok){
-                setFormData({ username: '',email: '', password: '' });
-                console.log('Registration Successful:');
-                setData(result);
-                setMsg(result.message);
+            if (response.ok) {
+                setFormData({ username: '', email: '', password: '' });
+                alert("Registration Successful, otp verrification required . Don't go back");
+                navigate('/register/send-otp', {
+                    state: { result },
+                });
             } else {
-                console.log('Registration failed:', result);   
-                setMsg("registration failed");
+                console.log('Registration failed:', result);
+                setMsg(result.errors);
             }
 
         } catch (error) {
             console.log('Error: ', error);
         }
-    // useEffect(() => {
-    //     // Define the URL of your backend API
-    //     const apiUrl = '/api/register';
+        // useEffect(() => {
+        //     // Define the URL of your backend API
+        //     const apiUrl = '/api/register';
 
-    //     // Fetch data from the backend
-    //     fetch(apiUrl)
-    //         .then((response) => {
-    //             if (!response.ok) {
-    //                 throw new Error('Network response was not ok');
-    //             }
-    //             return response.json();
-    //         })
-    //         .then((data) => {
-    //             setData(data);
-    //             setLoading(false);
-    //         })
-    //         .catch((error) => {
-    //             setError(error);
-    //             setLoading(false);
-    //         });
-    // }, []);
-    // console.log(data);
+        //     // Fetch data from the backend
+        //     fetch(apiUrl)
+        //         .then((response) => {
+        //             if (!response.ok) {
+        //                 throw new Error('Network response was not ok');
+        //             }
+        //             return response.json();
+        //         })
+        //         .then((data) => {
+        //             setData(data);
+        //             setLoading(false);
+        //         })
+        //         .catch((error) => {
+        //             setError(error);
+        //             setLoading(false);
+        //         });
+        // }, []);
+        // console.log(data);
     };
 
-    return(
+    return (
         <>
-        <h1 className="signup">SignUp</h1>
-        <p className="msg">{msg}</p>
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="text" 
-                id="username" 
-                placeholder="Username" 
-                value={formData.username}
-                onChange={handleChange}
+            <h1 className="signup">SignUp</h1>
+            <p className="msg">{msg}</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    id="username"
+                    placeholder="Username"
+                    value={formData.username}
+                    onChange={handleChange}
                 />
 
-            <input 
-                type="text" 
-                id="email" 
-                placeholder="Mail" 
-                value={formData.email}
-                onChange={handleChange}
+                <input
+                    type="text"
+                    id="email"
+                    placeholder="Mail"
+                    value={formData.email}
+                    onChange={handleChange}
                 />
 
-            <input 
-                type="password" 
-                id="password" 
-                placeholder="Password" 
-                value={formData.password}
-                onChange={handleChange}
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    onChange={handleChange}
                 />
 
-            <button 
-                className="btn" 
-                type="submit"
+                <button
+                    className="btn"
+                    type="submit"
                 >
                     Submit
                 </button>
-        </form>
+            </form>
 
-        
-    </>
+
+        </>
     )
 }
 
+function OtpResponse() {
+    const location = useLocation();
+    const m2 = location.state.result.data;
+    const {email} = m2;
+    const mail = {email};
+
+    const sendOtp = async () => {
+        try {
+            console.log(mail);
+            const response = await fetch('/api/register/send-otp', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(mail),
+            });
+
+            const result = await response.json();
+
+            console.log(result);
+            
 
 
-function FormOTP(){
+        } catch (error) {
+            console.log(error);
 
-    const [otp , setOtp] = useState({otp: ''})
-
-    const handleChange = (e) =>{
-        setOtp({...otp, [e.target.id]: e.target.value});
+        }
     }
 
-    const submitOtp = async(e) =>{
+    sendOtp();
+
+
+    // try {
+
+    //     const response = await fetch('/api/register/send-otp' , {
+    //         method: 'POST',
+    //         headers:{
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(message.email),
+    //     });
+
+    //     const result = await response.json();
+
+
+
+    // } catch (error) {
+    //     console.log("Error: ", error);
+    // }
+
+
+}
+
+
+function FormOTP() {
+
+    const [otp, setOtp] = useState({ otp: '' })
+
+
+    const handleChange = (e) => {
+        setOtp({ ...otp, [e.target.id]: e.target.value });
+    }
+
+    const submitOtp = async (e) => {
         e.preventDefault();
 
         try {
-            
+
             const response = await fetch("/api/register/otp", {
                 method: 'POST',
-                headers:{
+                headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(otp),
@@ -135,25 +190,25 @@ function FormOTP(){
         }
     }
 
-    return(
+    return (
         <form onChange={submitOtp}>
-            <input 
-                type="text" 
+            <input
+                type="text"
                 id="otp"
-                placeholder="Enter otp" 
+                placeholder="Enter otp"
                 value={otp.otp}
                 onChange={handleChange}
-                />
+            />
 
-            <button 
-                className="btn" 
+            <button
+                className="btn"
                 type="submit"
-                >
-                    Submit
-                </button>
+            >
+                Submit
+            </button>
         </form>
-        
+
     )
 }
 
-export  {Form, FormOTP};
+export { Form, FormOTP, OtpResponse };
