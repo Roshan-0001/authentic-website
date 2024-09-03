b1cf97637fb4aad791a3ec305e1416229347c498
Roshan-0001 <roshan.sharma6383@gmail.com>
2024-09-03 14:30:44 +0530

    home page update

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+
+
 function Homepage (){
     return(
-        <h1>---Home Page---</h1>
+        <>
+        <h1>--- Roshan Sharma ---</h1>
+        <a href="/register"><button className="btn signup" >SignUp</button></a>
+        <a href="#"><button className="btn login">Login</button></a>
+        </>
     )
 }
 
