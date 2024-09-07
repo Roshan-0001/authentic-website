63eb23c9df63cc5b6817dffc758f22b7f1422579
Roshan-0001 <roshan.sharma6383@gmail.com>
2024-09-07 20:59:15 +0530

    frontend proxy settings updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api':  {
-        target: 'https://auth-u-six.vercel.app/',
+        target: 'https://authentic-website.onrender.com/',
         // target: 'http://localhost:3000',
         changeOrigin: true,
       },
