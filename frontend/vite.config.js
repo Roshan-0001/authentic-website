7acdc4b9ad25e1ba090eb9744cd271ca2af6bbcc
Roshan-0001 <roshan.sharma6383@gmail.com>
2024-09-07 09:29:26 +0530

    update build commands and host link in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server:{
     proxy: {
       '/api':  {
-        target: 'http://localhost:3000',
+        target: 'https://auth-u-six.vercel.app/',
+        // target: 'http://localhost:3000',
         changeOrigin: true,
       },
       
