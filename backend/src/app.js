d01a9ec07ea3e761f3b26ead07a108252269cef7
Roshan-0001 <roshan.sharma6383@gmail.com>
2024-09-11 01:12:16 +0530

    cors setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,9 @@ const app = express();
 
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
-    credentials:true
+    credentials:true,
+    allowedHeaders: ['Content-Type', 'application/json'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
 app.use(express.json({limit:"16kb"}))
