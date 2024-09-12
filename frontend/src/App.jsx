2e1ac8c6c18f87180be55f85e5fc04612fe03b33
Roshan-0001 <roshan.sharma6383@gmail.com>
2024-09-12 12:13:30 +0530

    deleting some error files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,25 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-import { FormOTP , Form, OtpResponse } from './Form';
+import { FormOTP, Form, OtpResponse } from './Form';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Homepage from './Homepage';
-import  { LoginPage, OtpLogin, PasswordLogin } from './LoginPage';
-import { config } from 'dotenv';
+import { LoginPage, OtpLogin, PasswordLogin } from './LoginPage';
 
-config({
-  path: './.env'
-});
-
-const backUrl = import.meta.env.BACK_URL;
 
 function App() {
 
-  const [data, setData] = useState(null);
-
-  useEffect(()=>{
-    // axios.get('/api')
-    fetch(`${backUrl}/api`, {
-      method: "GET",
-      headers: {
-          'Content-Type': "application/json",
-      },
-  })
-      .then(response => setData(response.data))
-      .catch(error => console.log('Error fetching data: ', error));
-  }, []);
-
-
   return (
     <Router>
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
       <Routes>
         <Route path="*" element={<div>Page not found on this route </div>} />
-        <Route path="/" element={<Homepage/>} />
-        <Route path="/register" element={<Form/>} />
-        <Route path="/register/send-otp" element={<OtpResponse/>} />
-        <Route path="/register/verify-otp" element={<FormOTP/>} />
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/login/password-login' element={<PasswordLogin/>}/>
-        <Route path='/login/otp-login' element={<OtpLogin/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/register" element={<Form />} />
+        <Route path="/register/send-otp" element={<OtpResponse />} />
+        <Route path="/register/verify-otp" element={<FormOTP />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/login/password-login' element={<PasswordLogin />} />
+        <Route path='/login/otp-login' element={<OtpLogin />} />
       </Routes>
     </Router>
   );
